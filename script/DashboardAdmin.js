5fcec2b3e27be8ad2b8d860762880dca73f8550d
Vekktan <an.hafidzmaruf@gmail.com>
2023-11-30 19:26:10 +0700

    menambah beberapa assets, html, css, dan js

---
+++ script/DashboardAdmin.js
@@ -0,0 +1,4 @@
+function showContent(contentId) {
+    document.getElementById('contentDefault').classList.add('hidden');
+    document.getElementById(contentId).classList.remove('hidden');
+}
---
+++ script/TambahDestinasi.js
@@ -0,0 +1,7 @@
+document.getElementById('logoutButton').addEventListener('click', function() {
+    window.location.href = 'DashboardAdmin.html'; // Ganti dengan URL halaman awal yang sesuai
+});
+
+document.getElementById('batalUbah').addEventListener('click', function() {
+    window.location.href = 'DashboardAdmin.html'; // Ganti dengan URL halaman awal yang sesuai
+});
